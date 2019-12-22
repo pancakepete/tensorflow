1901323e44129de8d3f2faccb3e75aafd8a86c07
Piotr Witkiewicz <01032437@pw.edu.pl>
2019-12-22 20:34:40 +0100

    regression
    Signed-off-by: Piotr Witkiewicz <01032437@pw.edu.pl>

--- kaggle_housing.py
+++ kaggle_housing.py
@@ -14,5 +14,27 @@ raw_dataset = pd.read_csv(dataset_path,  sep=",")
 dataset = raw_dataset
 dataset.pop("Id")
 
-train_stats = dataset
+train_data = dataset
 train_labels = dataset.pop("SalePrice")
+
+## replacing nulls
+# categorical:
+train_data.select_dtypes(include='object').isnull().sum()[train_data.select_dtypes(include='object').isnull().sum() > 0]
+
+for col in ('Alley', 'BsmtQual', 'BsmtCond', 'BsmtExposure',
+            'BsmtFinType1', 'BsmtFinType2', 'FireplaceQu',
+            'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond',
+            'PoolQC', 'Fence', 'MiscFeature'):
+    train_data[col] = train_data[col].fillna('NA')
+
+for col in ('MasVnrType', 'Electrical'):
+    train_data[col] = train_data[col].fillna('None')
+
+# numerical:
+train_data.select_dtypes(include=['int', 'float']).isnull().sum()[train_data.select_dtypes(include=['int', 'float']).isnull().sum() > 0]
+
+for col in ('LotFrontage', 'MasVnrArea', 'GarageYrBlt'):
+    train_data[col] = train_data[col].fillna(0);
+
+## changing numerical data to categorical:
+train_data['MsSubClass']=train_data['MsSubClass'].astype(str)
