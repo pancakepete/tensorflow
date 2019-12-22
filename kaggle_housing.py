7734f905a11fce1edde41088bcf331bf1092f209
Piotr Witkiewicz <01032437@pw.edu.pl>
2019-12-22 20:49:11 +0100

    regression
    Signed-off-by: Piotr Witkiewicz <01032437@pw.edu.pl>

--- kaggle_housing.py
+++ kaggle_housing.py
@@ -9,15 +9,21 @@ import tensorflow as tf
 import os
 
 dataset_path = os.path.abspath("C:/Users/pwitk/Downloads/train.csv")
+test_dataset_path = os.path.abspath("C:/Users/pwitk/Downloads/test.csv")
 raw_dataset = pd.read_csv(dataset_path,  sep=",")
+raw_test_dataset = pd.read_csv(test_dataset_path, sep=",")
 
 dataset = raw_dataset
 dataset.pop("Id")
 
+test_dataset = raw_test_dataset
+test_dataset.pop("Id")
+test_data = test_dataset
+
 train_data = dataset
 train_labels = dataset.pop("SalePrice")
 
-## replacing nulls
+## replacing nulls in train data
 # categorical:
 train_data.select_dtypes(include='object').isnull().sum()[train_data.select_dtypes(include='object').isnull().sum() > 0]
 
@@ -36,5 +42,22 @@ train_data.select_dtypes(include=['int', 'float']).isnull().sum()[train_data.sel
 for col in ('LotFrontage', 'MasVnrArea', 'GarageYrBlt'):
     train_data[col] = train_data[col].fillna(0);
 
+## replacing nulls in test data
+# categorical
+test_data.select_dtypes(include='object').isnull().sum()[test_data.select_dtypes(include='object').isnull().sum() > 0]
+
+for col in ('MSZoning', 'Alley', 'BsmtQual', 'BsmtCond', 'BsmtExposure',
+            'BsmtFinType1', 'BsmtFinType2', 'FireplaceQu',
+            'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond',
+            'PoolQC', 'Fence', 'MiscFeature', 'KitchenQual'):
+    test_data[col] = test_data[col].fillna('NA')
+
+for col in ('MasVnrType', 'Electrical', 'Exteror1st', 'Exterior2nd', ):
+    test_data[col] = test_data[col].fillna('None')
+
+for col in ('Functional'):
+    test_data[col] = test_data[col].fillna('Typ')
+
 ## changing numerical data to categorical:
-train_data['MsSubClass']=train_data['MsSubClass'].astype(str)
+train_data['MSSubClass'] = train_data['MSSubClass'].astype(str)
+test_data['MSSubClass'] = test_data['MSSubClass'].astype(str)
