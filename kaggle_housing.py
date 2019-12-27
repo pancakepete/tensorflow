95ab99ff7a656d4c2fada0fa7539c1a6888b43e6
Piotr Witkiewicz <01032437@pw.edu.pl>
2019-12-27 15:41:43 +0100

    my first working regression model
    Signed-off-by: Piotr Witkiewicz <01032437@pw.edu.pl>

--- kaggle_housing.py
+++ kaggle_housing.py
@@ -1,10 +1,8 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import pathlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import tensorflow as tf
 import os
 
